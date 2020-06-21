d362a16d12f09137b8c96a96956cf24cfee17b3d
Will Fletcher <27767672+willflet@users.noreply.github.com>
2020-06-21 15:37:06 +0100

    personalization of teams

--- flammerouge/flammerouge/personalization.py
+++ flammerouge/flammerouge/personalization.py
@@ -2,9 +2,9 @@
 
 COLOUR_TO_NAMES = dict(
     red=dict(
-        team_name='Michaelton Scott',
-        rider_0_name='Adam Yates',
-        rider_1_name='Simon Yates'
+        team_name='Bing – Cillit Bang',
+        rider_0_name='Alejandro Valverde',
+        rider_1_name='Michael Matthews'
     ),
     blue=dict(
         team_name='Bosch – Lea & Perrins',
@@ -12,23 +12,23 @@ COLOUR_TO_NAMES = dict(
         rider_1_name='Tiesj Benoot'
     ),
     green=dict(
-        team_name='Team Specsavers – Visit Cornwall',
+        team_name='Specsavers – Visit Cornwall',
         rider_0_name='Rohan Dennis',
         rider_1_name='Sonny Colbrelli'
     ),
     black=dict(
         team_name='HP Morgan Stanley',
-        rider_0_name='Romain Bardet',
-        rider_1_name='Tony Gallopin'
+        rider_0_name='Tadej Pogačar',
+        rider_1_name='Tsgabu Grmay'
     ),
     pink=dict(
-        team_name='EF Education First Drapac powered by Cannondale',
-        rider_0_name='Rigoberto Urán Urán',
-        rider_1_name='Tejay van Garderen'
+        team_name='Toilet Duck – Duck Duck Go',
+        rider_0_name='Daniel Oss',
+        rider_1_name='Tony Martin'
     ),
     purple=dict(
-        team_name='Arkéa Samsic',
-        rider_0_name='Nairo Quintana',
-        rider_1_name='Nacer Bouhanni'
+        team_name='Ribena Racing',
+        rider_0_name='Alessandro De Marchi',
+        rider_1_name='Alessandro De Marchi'
     )
 )
